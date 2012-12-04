45f3eeedb342563def2d19ec7dae5101a1d520c2
Mikael Lepistö <mikael.lepisto@vincit.fi>
2012-12-04 10:08:32 +0200

    Added test about pointing constant memory address

---
+++ tests/test_direct_memeory_access.c
@@ -0,0 +1,17 @@
+/**
+ * We should either assert or handle this case.
+ */
+
+// RUN: clang -c $TEST_SRC -O1 -emit-llvm -o $OUT_FILE.bc &&
+// RUN: echo "Check that unsupported code cannot be compiled" &&
+// RUN: (
+// RUN:   opt -load $CLAMP_PLUGIN -clamp-pointers -internalize -internalize-public-api-list=main -globaldce -S $OUT_FILE.bc -o $OUT_FILE.clamped.ll ; 
+// RUN:     ([ $? != 0 ] && echo "Compilation aborted as expected") || 
+// RUN:     (echo "Clamping should have aborted unsupported case, however pass resulted:" && cat $OUT_FILE.clamped.ll && false) 
+// RUN: ) 
+
+extern int ext_val;
+
+int main(void) {
+  return *((int*)0x00ff00ff);
+}
