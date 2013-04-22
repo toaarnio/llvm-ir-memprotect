67314dd90f16c4257acc23405a1364cc7b416a13
Erkki Seppälä <erkki.seppala@vincit.fi>
2013-04-22 11:40:56 +0300

    run_tests.sh: bash compatibility fix
    -z requires zero or one argument, added quoting to ensure that

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -76,7 +76,7 @@ for test_file in $tests;do
     run_test $test_file;
 done;
 
-if [ ! -z $failed_tests ]; then
+if [ ! -z "$failed_tests" ]; then
     echo "###################### FAIL ###################################";
     echo "## Failed tests: $failed_tests";
 else
