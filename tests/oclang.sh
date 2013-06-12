d1ad7e5c23c70677481fa0fb2f233f3c7bcfdd06
Erkki Seppälä <erkki.seppala@vincit.fi>
2013-06-12 15:13:57 +0300

    oclang: added support for using libclc headers
    Use by setting the environment variable USE_CLC=1 when running $OCLANG
    in test cases.

--- tests/oclang.sh
+++ tests/oclang.sh
@@ -8,6 +8,21 @@ if [ -z  "$TARGET_FLAGS" ]; then
 TARGET_FLAGS="-include $SCRIPT_PATH/pocl_kernel.h -Dcles_khr_int64 -Dcl_khr_fp16 -Dcl_khr_fp64"
 fi
 
+if [ "x$USE_CLC" = "x1" ]; then
+  if [ -z "$LIBCLC" ]; then 
+    TOPDIR=$PWD
+    while [ -n "$TOPDIR" -a ! -d $TOPDIR/libclc ]; do
+      TOPDIR="`echo $TOPDIR | sed 's,/[^/]*$,,'`"
+    done
+    if [ -z "$TOPDIR" ]; then
+	echo "Cannot find libclc. Set environment variable LIBCLC to point to its source directory."
+	exit 1
+    fi
+    LIBCLC=$TOPDIR/libclc
+  fi
+  TARGET_FLAGS="-I$LIBCLC/generic/include -include $LIBCLC/generic/include/clc/clc.h -Dcl_clang_storage_class_specifiers"
+fi
+
 if [ "x$BUILDING_RUNKERNEL" = x1 ]; then 
     TARGET_FLAGS="-DBUILDING_RUNKERNEL=1 $TARGET_FLAGS"
 fi
